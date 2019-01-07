c8d21700278d7b3da4413e805c030ef1e8ebf8d9
johnvmt <johnvmt@gmail.com>
2019-01-06 21:39:05 -0500

    v7.0.1 * Removed check for purge function, as PouchDB does not set the function immediately

--- module.js
+++ module.js
@@ -3,21 +3,17 @@
 exports.purgeOnDelete = function() {
 	const db = this;
 
-	if(typeof db.purge !== 'function')
-		throw new Error('purge function does not exist');
-	else {
-		db.changes({
-			since: 'now',
-			live: true,
-			include_docs: true
-		}).on('change', function (change) {
-			if (change.deleted && typeof db.purge == 'function')
-				db.purge(change.doc);
+	db.changes({
+		since: 'now',
+		live: true,
+		include_docs: true
+	}).on('change', function (change) {
+		if(change.deleted && typeof db.purge == 'function')
+			db.purge(change.doc);
 
-		}).on('error', function (err) {
-			// handle errors
-		});
-	}
+	}).on('error', function (err) {
+		// handle errors
+	});
 };
 
 /* istanbul ignore next */
